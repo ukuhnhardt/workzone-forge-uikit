259e1da4c3563217fb21e970238e24d9e7149c12
ulrich kuhnhardt <ulrich@izymes.com>
2025-07-08 15:01:01 +1000

    api.asUser(ACCOUNT_ID).requestBitbucket works

--- src/triggers/index.js
+++ src/triggers/index.js
@@ -1,12 +1,39 @@
 import api, {fetch as forgeFetch, route} from "@forge/api";
 import {invokeRemote} from "@forge/api";
 
+
+async function mergeAsUserByAccountId(workspace, repository, pullRequest) {
+  const ACCOUNT_ID = process.env["ACCOUNT_ID"] ?? "557058:de2deb41-6382-4be7-aa31-4adf2439211f";
+  console.log("mergeAsUserByAccountId: ACCOUNT_ID", ACCOUNT_ID);
+  const memberPermissions = await api.asApp().requestBitbucket(route`/2.0/workspaces/${workspace.uuid}/permissions`)
+  console.log("memberPermissions", JSON.stringify(await memberPermissions.json(), null, 2));
+  const mergeResponse = await api.asUser(ACCOUNT_ID).requestBitbucket(
+    route`/2.0/repositories/${workspace.uuid}/${repository.uuid}/pullrequests/${pullRequest.id}/merge`, {
+      method: 'POST',
+      body: `{"type" : ""}`,
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Basic ${upw}`
+      }
+    })
+  const data = await mergeResponse.json();
+  const status = mergeResponse.status;
+  console.log("mergePullRequest:asApp mergeResponse data", status, mergeResponse.statusText, JSON.stringify(data, null, 2));
+
+}
+
 export const pullRequestTriggerHandler = async (event, context) => {
   console.log("pullRequestTriggerHandler: event", event.eventType, JSON.stringify(event, null, 2));
   const { workspace, repository, pullrequest } = event;
-  console.log("pullRequestTriggerHandler: merge checks url", `/!api/internal/repositories/${workspace.uuid}/${repository.uuid}/pullrequests/${pullrequest.id}/merge-restrictions`);
+  // await checkMergeRestrictionsInternalApi(workspace, repository, pullrequest);
+  
+  await mergeAsUserByAccountId(workspace, repository, pullrequest);
+}
+
+async function checkMergeRestrictionsInternalApi(workspace, repository, pullrequest) {
+  console.log("pullRequestTriggerHandler: merge restrictions url", `/!api/internal/repositories/${workspace.uuid}/${repository.uuid}/pullrequests/${pullrequest.id}/merge-restrictions`);
   const mergeChecks = await api.asApp().requestBitbucket(route`/!api/internal/repositories/${workspace.uuid}/${repository.uuid}/pullrequests/${pullrequest.id}/merge-restrictions`)
-  console.log("pullRequestTriggerHandler: mergeChecks", mergeChecks);
+  console.log("pullRequestTriggerHandler: merge-restrictions", mergeChecks);
   if (mergeChecks.status < 400) {
     return;
   }
